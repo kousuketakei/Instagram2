058e55d6800202ebff16bc4cc55b8fd1bf3143f7
kousuketakei <tom20100320forever@gmail.com>
2019-11-26 21:56:06 +0900

    コミット一回目

---
+++ Instagram2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram2
+//
+//  Created by 武井康祐 on 2019/11/26.
+//  Copyright © 2019 KousukeTakei. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
