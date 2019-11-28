e054b24eeb350d1e2b6c65c2a0f4815ec75a9d33
kousuketakei <tom20100320forever@gmail.com>
2019-11-28 20:05:04 +0900

    ESTabBarControllerの初期設定

---
+++ Instagram2/SettingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingViewController.swift
+//  Instagram2
+//
+//  Created by 武井康祐 on 2019/11/27.
+//  Copyright © 2019 KousukeTakei. All rights reserved.
+//
+
+import UIKit
+
+class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
